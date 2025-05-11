39045d507918f052816643fa1adfd7f5b95c0e63
sonali <sonali@sonali.com>
2025-05-12 01:34:17 +0530

    Initial commit

---
+++ src/main/java/com/baeldung/freemarker/controller/SpringController.java
@@ -0,0 +1,104 @@
+package com.baeldung.freemarker.controller;
+
+
+import jakarta.servlet.http.HttpSession;
+import java.util.*;
+
+//import com.baeldung.freemarker.method.LastCharMethod;
+// import freemarker.template.DefaultObjectWrapperBuilder;
+// import freemarker.template.Version;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+// ======= This added by me =========
+// import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+
+import com.baeldung.freemarker.model.Car;
+
+@Controller
+public class SpringController {
+
+    private static List<Car> carList = new ArrayList<Car>();
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        return "redirect:/cars";
+    }
+
+   // static {
+     //   carList.add(new Car("Honda", "Civic"));
+       // carList.add(new Car("Toyota", "Camry"));
+       // carList.add(new Car("Nissan", "Altima"));
+   // }
+
+    @RequestMapping(value = "/cars", method = RequestMethod.GET)
+    public String init(@ModelAttribute("model") ModelMap model) {
+        model.addAttribute("carList", carList);
+        return "index";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addCar(@ModelAttribute("car") Car car) {
+        if (null != car && null != car.getMake() && null != car.getModel() && !car.getMake().isEmpty() && !car.getModel().isEmpty()) {
+            carList.add(car);
+        }
+        return "redirect:/cars";
+    }
+
+   
+
+
+
+    // Show the commons page and display last added car and login info
+    @RequestMapping(value = "/cars/commons", method = RequestMethod.GET)
+    public String showCommonsPage(HttpSession session, Model model) {
+        // Retrieve login info from session
+        Object username = session.getAttribute("make");
+        Object password = session.getAttribute("model");
+
+        // Add the login info to the model if available
+        if (username != null && password != null) {
+            model.addAttribute("username", username);
+            model.addAttribute("password", password);
+        }
+        
+        System.out.println("Showing Car list:" + carList);
+        // Retrieve the last added car (if available)
+        if (!carList.isEmpty()) {
+            Car lastCar = carList.get(carList.size() - 1);  // Get the last car added
+            System.out.println("last Element In Carlist: " + lastCar);
+            model.addAttribute("lastCar", lastCar);  // Add the last car to the model
+        }
+
+        return "commons";  // Return the commons view
+    }
+
+
+
+    // Save username and password to session and redirect to the commons page
+    @RequestMapping(value = "/cars/commons", method = RequestMethod.POST)
+    public String showCommonsInfo(@ModelAttribute("car") Car car, HttpSession session) {
+        String username = car.getMake(); // Assuming car make is the username
+        String password = car.getModel(); // Assuming car model is the password
+        
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+            // Save login info to session
+            session.setAttribute("username", username);
+            session.setAttribute("password", password);
+        }
+
+        return "redirect:/cars/commons";  // Redirect to GET method to display login info and last car
+    }
+
+    
+}
---
+++ src/main/webapp/WEB-INF/views/ftl/commons.ftl
@@ -0,0 +1,50 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Commons Page</title>
+</head>
+<body>
+    <h2>Login Info</h2>
+
+    <#if lastCar.make?? && lastCar.model??>
+        <p>Your login name is: <b>${lastCar.make}</b></p>
+        <p>Your password is: <b>${lastCar.model}</b></p>
+    <#else>
+        <p>No login info submitted.</p>
+    </#if>
+</body>
+</html>
+
+
+
+  
+<#--  <p>Testing is a property exists: ${student???c}</p>
+<#if status??>
+    <p>Property value: ${status.reason}</p>
+<#else>
+    <p>Missing property</p>
+</#if>
+
+<p>Iterating a sequence:</p>
+<#list statuses>
+    <ul>
+        <#items as status>
+        <li>${status}</li>
+    </#items>
+    </ul>
+    <#else>
+    <p>No statuses available</p>
+</#list>
+
+<p>Using static methods</p>
+<#assign MathUtils=statics['java.lang.Math']>
+<p>PI value: ${MathUtils.PI}</p>
+<p>2*10 is: ${MathUtils.pow(2, 10)}</p>
+
+<p>Using bean method</p>
+<p>Random value: ${random.nextInt()}</p>
+
+<p>Use custom method</p>
+<p>Last char example: ${lastChar('mystring')}</p>  -->
